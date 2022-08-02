864b1dff614744b990d894bada0f3de44f1a8842
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-08-02 16:15:46 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/Main.java
+++ src/main/java/com/trybe/acc/java/Main.java
@@ -1,10 +1,39 @@
 package com.trybe.acc.java;
 
+import java.util.Scanner;
+
+/** Classe principal. */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  /** Método principal. */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-	}
+    System.out.println("Qual é o seu nome?");
+    final String nome = scan.next();
+
+    System.out.println("Entre com as notas das provas:");
+    System.lineSeparator();
+
+    System.out.println("Prova 1:");
+    final double p1 = scan.nextDouble();
+    System.out.println("Prova 2:");
+    final double p2 = scan.nextDouble();
+    System.out.println("Prova 3:");
+    final double p3 = scan.nextDouble();
+    System.out.println("Prova 4:");
+    final double p4 = scan.nextDouble();
+
+    scan.close();
+
+    String media = Double.toString(Nota.calcularMedia(p1, p2, p3, p4));
+
+    StringBuilder stringBuilder = new StringBuilder();
+    StringBuilder resultado = stringBuilder.append("Olá, ")
+                                           .append(nome)
+                                           .append(" sua média é ")
+                                           .append(media);
+    System.out.print(resultado);
+  }
 
 }
--- src/main/java/com/trybe/acc/java/Nota.java
+++ src/main/java/com/trybe/acc/java/Nota.java
@@ -1,5 +1,14 @@
 package com.trybe.acc.java;
 
+
+/** Classe Nota dos alunos. */
 public class Nota {
+  
+  /** Média anual dos alunos. */
+  public static double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
+    double calculo = (nota1 + nota2 + nota3 + nota4) / 4;
+    
+    return calculo;
+  }
 
 }
